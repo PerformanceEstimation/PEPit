1089419255ea53f862f7b647c9657e9c0975d1c6
Adrien <adrien.taylor@gmail.com>
2021-08-31 08:55:00 +0200

    Base Algo steps

---
+++ PEPit/Base_algorithmic_operations/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['bregmangradient_step','bregmanproximal_step','exactlinesearch_step','gradient_step','inexactbregmanproximal_step','inexactgradient_step','inexactproximal_step','linearoptimization_step','proximal_step']
