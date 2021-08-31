0cc68d33a4b61ba45a94665a7f29a37027a33b2e
Adrien <adrien.taylor@gmail.com>
2021-08-31 16:15:24 +0200

    prox grad

---
+++ Examples/B_MethodsForCompositeConvexMinimization/a_proximalgradient.py
@@ -0,0 +1,65 @@
+from PEPit.pep import PEP
+from PEPit.Function_classes.strong_cvx_smooth_function import StrongCvxSmoothFunction
+from PEPit.Function_classes.cvx_function import CvxFunction
+from PEPit.Primitive_steps.proximal_step import proximal_step
+
+
+def wc_pgd(L, mu, gamma, n):
+    """
+    DEF PROBLEM (minimisation fct convexe)
+
+    DEF ALGO
+
+    DEF PIRE CAS
+
+    REF avec solution analytique?
+
+    :param L:
+    :param mu:
+    :param gamma:
+    :param n:
+    :return:
+    """
+
+    # Instantiate PEP
+    problem = PEP()
+
+    # Declare a strongly convex smooth function
+    f1 = problem.declare_function(StrongCvxSmoothFunction, {'mu': mu, 'L': L})
+    f2 = problem.declare_function(CvxFunction, {})
+    func = f1 + f2
+
+    # Start by defining its unique optimal point
+    xs = func.optimal_point()
+
+    # Then Define the starting point of the algorithm
+    x0 = problem.set_initial_point()
+
+    # Set the initial constraint that is the distance between x0 and x^*
+    problem.set_initial_condition((x0 - xs) ** 2 <= 1)
+
+    # Run the GD method
+    x = x0
+    for _ in range(n):
+        y = x - gamma * f1.gradient(x)
+        x, _, _ = proximal_step(y, f2, gamma)
+
+    # Set the performance metric to the final distance to optimum
+    problem.set_performance_metric((x - xs)**2)
+
+    # Solve the PEP
+    wc = problem.solve()
+
+    # Return the worst-case rate of the method, which should be max((1-gamma*mu)^2,(1-gamma*L)^2)
+    return wc
+
+
+if __name__ == "__main__":
+    n = 2
+    L = 1
+    mu = .1
+    gamma = 1
+
+    rate = wc_pgd(L, mu, gamma, n)
+
+    print('{}'.format(rate))
