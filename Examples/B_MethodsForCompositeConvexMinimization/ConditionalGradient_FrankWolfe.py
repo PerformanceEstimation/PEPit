6d8e2ebb39af46dd84e006ee41d3889526ebbb48
M-Celine <celine.moucer@gmail.com>
2021-10-05 17:12:31 +0200

    Algo update

--- Examples/B_MethodsForCompositeConvexMinimization/ConditionalGradient_FrankWolfe.py
+++ Examples/B_MethodsForCompositeConvexMinimization/ConditionalGradient_FrankWolfe.py
@@ -1,10 +1,10 @@
 import numpy as np
-import cvxpy as cp
 
 from PEPit.pep import PEP
 from PEPit.Function_classes.smooth_convex_function import SmoothConvexFunction
 from PEPit.Function_classes.convex_indicator import ConvexIndicatorFunction
 from PEPit.Primitive_steps.linearoptimization_step import linearoptimization_step
+
 
 def wc_cg_fw(L, D, n):
     """
@@ -31,15 +31,14 @@ def wc_cg_fw(L, D, n):
     :return:
     """
 
-
     # Instantiate PEP
     problem = PEP()
 
     # Declare a convex lipschitz function
     func1 = problem.declare_function(SmoothConvexFunction,
-                                    {'L': L})
+                                     {'L': L})
     func2 = problem.declare_function(ConvexIndicatorFunction,
-                                     {'D': D, 'R': np.inf})
+                                     {'D': D})
     func = func1 + func2
 
     # Start by defining its unique optimal point and its function value
@@ -48,29 +47,28 @@ def wc_cg_fw(L, D, n):
 
     # Then Define the starting point of the algorithm
     x0 = problem.set_initial_point()
-    _ = func2.value(x0)
 
     # Set the initial constraint that is the distance between x0 and x^*
-    problem.set_initial_condition((x0 - xs)**2 <= 1)
+    problem.set_initial_condition((x0 - xs) ** 2 <= 1)
 
     # Compute trajectory starting from x0
     x = x0
     for i in range(n):
         g = func1.gradient(x)
         y, _, _ = linearoptimization_step(g, func2)
-        lam = 2/(i+2)
-        x = (1-lam) * x + lam * y
+        lam = 2 / (i + 1)
+        x = (1 - lam) * x + lam * y
 
     # Set the performance metric to the final distance to optimum
-    problem.set_performance_metric((func1.value(x)) - fs)
+    problem.set_performance_metric((func.value(x)) - fs)
 
     # Solve the PEP
-    wc = problem.solve(cp.MOSEK)
+    wc = problem.solve()
 
     # Theoretical guarantee (for comparison)
     # when theta = 1
-    theory = 2 * L * D**2 / (n+2)
-    print('*** Example file: worst-case performance of the Conditional Gradient / Frank-Wolfe in function value ***')
+    theory = 2 * L * D ** 2 / (n + 2)
+    print('*** Example file: worst-case performance of the Conditional Gradient (Franck-Wolfe) in function value ***')
     print('\tPEP-it guarantee:\tf(y_n) - f_* <= ', wc)
     print('\tTheoretical standard guarantee :\tf(y_n) - f_* <=  <= ', theory)
     # Return the worst-case guarantee of the evaluated method (and the upper theoretical value)
@@ -78,8 +76,8 @@ def wc_cg_fw(L, D, n):
     # Return the rate of the evaluated method
     return wc
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     D = 1.
     L = 1.
     n = 10
