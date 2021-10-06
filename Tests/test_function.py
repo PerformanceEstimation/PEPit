9ae1b5a38639317837333f9fb754c25cacf864cd
baptiste <baptiste.goujaud@gmail.com>
2021-10-06 18:33:02 +0200

    Create some new tests

--- Tests/test_function.py
+++ Tests/test_function.py
@@ -94,6 +94,27 @@ class TestFunction(unittest.TestCase):
         self.assertEqual((-grad1 + 9 * grad2 / 5).decomposition_dict, grad.decomposition_dict)
         self.assertEqual(prune_dict((-val1 + 9 * val2 / 5).decomposition_dict), val.decomposition_dict)
 
+    def test_oracle_with_predetermined_values(self):
+
+        new_function = self.compute_linear_combination()
+        new_function.oracle(point=self.point)
+
+        grad1, val1 = self.func1.oracle(point=self.point)
+        grad2, val2 = self.func2.oracle(point=self.point)
+
+        grad, val = new_function.oracle(point=self.point)
+
+        self.assertEqual(prune_dict(val.decomposition_dict), prune_dict((-val1 + 9/5*val2).decomposition_dict))
+        print(self.func1.list_of_points)
+        self.assertNotEqual(prune_dict(grad.decomposition_dict), prune_dict((-grad1 + 9/5*grad2).decomposition_dict))
+        # self.assertEqual(len(self.func1.list_of_points), 2)
+        # self.assertEqual(len(self.func2.list_of_points), 2)
+        other_grad1, other_val1 = self.func1.list_of_points[2][1:]
+        other_grad2, other_val2 = self.func2.list_of_points[2][1:]
+        self.assertEqual(val1.decomposition_dict, other_val1.decomposition_dict)
+        self.assertEqual(val2.decomposition_dict, other_val2.decomposition_dict)
+        self.assertEqual(prune_dict(grad.decomposition_dict), prune_dict((-other_grad1 + 9/5*other_grad2).decomposition_dict))
+
     def test_optimal_point(self):
 
         new_function = self.compute_linear_combination()
