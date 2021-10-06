7e0878c2bb431412da536e55cbe0e70092c2fd88
baptiste <baptiste.goujaud@gmail.com>
2021-10-06 19:51:01 +0200

    Replace actual warning by a more explicit message

--- PEPit/pep.py
+++ PEPit/pep.py
@@ -1,5 +1,3 @@
-import warnings
-
 import numpy as np
 import cvxpy as cp
 
@@ -234,8 +232,12 @@ class PEP(object):
 
         # Verify negative eigenvalues are only precision mistakes and get rid of negative eigenvalues
         if np.min(eig_val) < 0:
-            warnings.warn("Not all the eigenvalues of the Gram matrix are nonnegative: the smallest is equal to {:.3}. "
-                          "The negative ones are replaced by 0.".format(np.min(eig_val)))
+            print("\033[93m(PEP-it) Postprocessing: solver\'s output is not entirely feasible"
+                  " (smallest eigenvalue of the Gram matrix is: {:.3} < 0).\n"
+                  " Small deviation from 0 may simply be due to numerical error."
+                  " Big ones should be deeply investigated.\n"
+                  " In any cases, from now the provided values of parameters are based on the projection of the Gram"
+                  " matrix onto the cone of symmetric semi-definite matrix.\033[0m".format(np.min(eig_val)))
             eig_val = np.maximum(eig_val, 0)
 
         # Extracts points values
