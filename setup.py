8b19948efd7a51aa2c70c585e8a77a53361c80ab
baptiste <baptiste.goujaud@gmail.com>
2021-11-09 19:27:39 +0100

    Update setting file for pypi upload

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="PEPit",
     version="0.0.1",
-    author="TBC",
-    author_email="TBC",
-    description="A small example package",
+    author="Baptiste Goujaud, Céline Moucer, Julien Hendrickx, Francois Glineur, Adrien Taylor and Aymeric Dieuleveut",
+    author_email="baptiste.goujaud@gmail.com",
+    description="PEPit is a package that allows users to pep their optimization algorithms as easy as they code them",
     long_description=long_description,
     long_description_content_type="text/markdown",
     setup_requires=["numpy", "scipy", "cvxpy"],
     url="https://github.com/bgoujaud/PEPit",
     project_urls={
-        "Documentation": "https://github.com/bgoujaud/PEPit/documentation",
+        "Documentation": "https://github.com/bgoujaud/PEPit/docs",
     },
+    download_url='https://github.com/bgoujaud/PEPit/archive/refs/tags/0.0.1.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
