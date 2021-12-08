293ed92c47e79c47af344ace8a50d9ba8ac8f856
baptiste <baptiste.goujaud@gmail.com>
2021-12-08 20:04:35 +0100

    docstring for proximal point on operator

--- PEPit/examples/e_monotone_inclusions/proximal_point_method.py
+++ PEPit/examples/e_monotone_inclusions/proximal_point_method.py
@@ -6,25 +6,63 @@ from PEPit.primitive_steps.proximal_step import proximal_step
 def wc_ppm(alpha, n, verbose=True):
     """
     Consider the monotone inclusion problem
-        Find x, 0 \in Ax,
-    where A is maximally monotone. we denote JA the resolvents of A.
 
-    This code computes a worst-case guarantee for the proximal point method, that is the smallest
-    possible tau(n,L) such that the guarantee
-        ||x_(n) - x(n-1)||^2 <= tau(n,L) * ||x_0 - x_\star||^2,
-    is valid, where x_\star is such that 0 \in Ax_\star.
+        .. math:: \\text{Find} \ x, 0 \in Ax,
 
-    Theoretical rates can be found in the following paper (section 4)
+    where :math:`A` is maximally monotone. We denote :math:`J_A = (I + A)^{-1}` the resolvents of :math:`A`.
 
-    [1] Guoyong Gu, and Junfeng Yang. "Optimal nonergodic sublinear
-    convergence rate of proximal point algorithm for maximal monotone
-    inclusion problems." (2019)
+    This code computes a worst-case guarantee for the **proximal point** method,
+    that is the smallest possible :math:`\\tau(n, \\alpha)` such that the guarantee
 
-    :param alpha: (float) the step size
-    :param n: (int) number of iterations.
-    :param verbose: (bool) if True, print conclusion
+        .. math:: ||x_n - y_n||^2 \\leqslant \\tau(n, \\alpha) ||x_0 - x_\star||^2,
 
-    :return: (tuple) worst_case value, theoretical value
+    is valid, where :math:`x_\star` is such that :math:`0 \\in Ax_\star`.
+
+    **Algorithm**:
+
+        .. math::
+
+            \begin{eqnarray}
+                x_{i+1} & = & J_{\\alpha A}(y_i) \\\\
+                y_{i+1} & = & x_{i+1} + \\frac{i}{i+2}(x_{i+1} - x_{i}) - \\frac{i}{i+1}(x_i - y_{i-1})
+            \bend{eqnarray}
+
+    **Theoretical guarantee**:
+
+    Theoretical rates can be found in [1, section 4].
+
+        .. math:: \\|x_n - x_{n-1}\\|^2 \\leqslant  \\frac{\\left(1 - \\frac{1}{n}\\right)^{n - 1}}{n} \\|x_0 - x_\star\\|^2
+
+    **Reference**:
+
+        Theoretical rates can be found in [1, section 4].
+
+        [1] Guoyong Gu, and Junfeng Yang. "Optimal nonergodic sublinear
+        convergence rate of proximal point algorithm for maximal monotone
+        inclusion problems." (2019)
+
+    Args:
+        alpha (float): the step size
+        n (int): number of iterations.
+        verbose (bool, optional): if True, print conclusion
+
+    Returns:
+        tuple: worst_case value, theoretical value
+
+    Example:
+        >>> pepit_tau, theoretical_tau = wc_ppm(alpha=2, n=10, verbose=True)
+        (PEP-it) Setting up the problem: size of the main PSD matrix: 12x12
+        (PEP-it) Setting up the problem: performance measure is minimum of 1 element(s)
+        (PEP-it) Setting up the problem: initial conditions (1 constraint(s) added)
+        (PEP-it) Setting up the problem: interpolation conditions for 1 function(s)
+                 function 1 : 110 constraint(s) added
+        (PEP-it) Compiling SDP
+        (PEP-it) Calling SDP solver
+        (PEP-it) Solver status: optimal (solver: SCS); optimal value: 0.03874199421010509
+        *** Example file: worst-case performance of the Proximal Point Method***
+            PEP-it guarantee:		 ||x(n) - x(n-1)||^2 <= 0.038742 ||x0 - xs||^2
+            Theoretical guarantee:	 ||x(n) - x(n-1)||^2 <= 0.038742 ||x0 - xs||^2
+
     """
 
     # Instantiate PEP
@@ -43,12 +81,13 @@ def wc_ppm(alpha, n, verbose=True):
     problem.set_initial_condition((x0 - xs) ** 2 <= 1)
 
     # Compute n steps of the Proximal Gradient method starting from x0
-    x = [x0 for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        x[i], _, _ = proximal_step(x[i - 1], A, alpha)
+    x = x0
+    for _ in range(n):
+        previous_x = x
+        x, _, _ = proximal_step(previous_x, A, alpha)
 
     # Set the performance metric to the distance between x(n) and x(n-1)
-    problem.set_performance_metric((x[n] - x[n - 1]) ** 2)
+    problem.set_performance_metric((x - previous_x) ** 2)
 
     # Solve the PEP
     pepit_tau = problem.solve(verbose=verbose)
@@ -59,16 +98,13 @@ def wc_ppm(alpha, n, verbose=True):
     # Print conclusion if required
     if verbose:
         print('*** Example file: worst-case performance of the Proximal Point Method***')
-        print('\tPEP-it guarantee:\t ||x(n) - x(n-1)||^2 <= {:.6} ||x0 - xs||^2'.format(pepit_tau))
-        print('\tTheoretical guarantee :\t ||x(n) - x(n-1)||^2 <= {:.6} ||x0 - xs||^2 '.format(theoretical_tau))
+        print('\tPEP-it guarantee:\t\t ||x(n) - x(n-1)||^2 <= {:.6} ||x0 - xs||^2'.format(pepit_tau))
+        print('\tTheoretical guarantee:\t ||x(n) - x(n-1)||^2 <= {:.6} ||x0 - xs||^2 '.format(theoretical_tau))
 
     # Return the worst-case guarantee of the evaluated method ( and the reference theoretical value)
     return pepit_tau, theoretical_tau
 
 
 if __name__ == "__main__":
-    alpha = 2
-    n = 3
 
-    pepit_tau, theoretical_tau = wc_ppm(alpha=alpha,
-                                        n=n)
+    pepit_tau, theoretical_tau = wc_ppm(alpha=2, n=10, verbose=True)
