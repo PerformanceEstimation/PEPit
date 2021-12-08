a401dd80477508b93c68879251043465e44520bf
baptiste <baptiste.goujaud@gmail.com>
2021-12-08 20:14:51 +0100

    docstring for gd in non convex

--- PEPit/examples/c_methods_for_nonconvex_optimization/gradient_method.py
+++ PEPit/examples/c_methods_for_nonconvex_optimization/gradient_method.py
@@ -14,19 +14,51 @@ def wc_gd(L, gamma, n, verbose=True):
     That is, it computes the smallest possible :math:`\\tau(n, L, \\gamma)` such that the guarantee
 
     .. math:: \\min_{t\\leqslant n} \\|\\nabla f(x_t)\\|^2 \\leqslant \\tau(n, L, \\gamma) (f(x_0) - f(x_n))
-    is valid, where x_n is the output of the gradient descent with fixed step sizes,
-    and where x_n is the n-th iterates obtained with the gradient method.
 
-    The detailed approach is available in [1].
-    [1] Polyak, Boris. "Introduction to optimization" (1987)
-    and the theoretical upper bound is given in Theorem 1.
+    is valid, where :math:`x_n` is the n-th iterates obtained with the gradient method with fixed step size.
 
-    :param L: (float) the smoothness parameter.
-    :param gamma: (float) the step size.
-    :param n: (int) number of iterations.
-    :param verbose: (bool) if True, print conclusion
+    **Algorithm**:
+    Gradient descent is described by
 
-    :return: (tuple) worst_case value, theoretical value
+    .. math:: x_{k+1} = x_k - \\gamma \\nabla f(x_k),
+
+    where :math:`\\gamma` is a step size.
+
+    **Theoretical guarantee**:
+    When :math:`\\gamma \\leqslant \\frac{1}{L}`, the tight theoretical guarantee can be found in [1, Theorem 1]:
+
+    .. math:: \\min_{t\\leqslant n} \\|\\nabla f(x_t)\\|^2 \\leqslant \\frac{4}{3}\\frac{L}{n} (f(x_0) - f(x_n))
+
+    References:
+
+        The detailed approach is available in [1].
+
+        [1] Polyak, Boris. "Introduction to optimization" (1987)
+
+    Args:
+        L (float): the smoothness parameter.
+        gamma (float): step size.
+        n (int): number of iterations.
+        verbose (bool): if True, print conclusion
+
+    Returns:
+        tuple: worst_case value, theoretical value
+
+    Example:
+        >>> L = 1
+        >>> pepit_tau, theoretical_tau = wc_gd(L=L, gamma=1 / L, n=5, verbose=True)
+        (PEP-it) Setting up the problem: size of the main PSD matrix: 7x7
+        (PEP-it) Setting up the problem: performance measure is minimum of 6 element(s)
+        (PEP-it) Setting up the problem: initial conditions (1 constraint(s) added)
+        (PEP-it) Setting up the problem: interpolation conditions for 1 function(s)
+                 function 1 : 30 constraint(s) added
+        (PEP-it) Compiling SDP
+        (PEP-it) Calling SDP solver
+        (PEP-it) Solver status: optimal (solver: SCS); optimal value: 0.2666769474847614
+        *** Example file: worst-case performance of gradient descent with fixed step size ***
+            PEP-it guarantee:		 min_i (f'(x_i)) ** 2 <= 0.266677 (f(x_0)-f_*)
+            Theoretical guarantee:	 min_i (f'(x_i)) ** 2 <= 0.266667 (f(x_0)-f_*)
+
     """
 
     # Instantiate PEP
@@ -42,17 +74,18 @@ def wc_gd(L, gamma, n, verbose=True):
     # Run n steps of GD method with step size gamma
     x = x0
     gx, fx = g0, f0
-    for i in range(n):
-        x = x - gamma * gx
-        # Set the performance metric to the minimum of the gradient norm over the iterations
-        problem.set_performance_metric(gx ** 2)
-        gx, fx = func.oracle(x)
-
-    # Set the initial constraint that is the difference between fN and f0
-    problem.set_initial_condition(f0 - fx <= 1)
 
     # Set the performance metric to the minimum of the gradient norm over the iterations
     problem.set_performance_metric(gx ** 2)
+
+    for i in range(n):
+        x = x - gamma * gx
+        # Set the performance metric to the minimum of the gradient norm over the iterations
+        gx, fx = func.oracle(x)
+        problem.set_performance_metric(gx ** 2)
+
+    # Set the initial constraint that is the difference between fN and f0
+    problem.set_initial_condition(f0 - fx <= 1)
 
     # Solve the PEP
     pepit_tau = problem.solve(verbose=verbose)
@@ -71,11 +104,6 @@ def wc_gd(L, gamma, n, verbose=True):
 
 
 if __name__ == "__main__":
-    n = 5
+
     L = 1
-    gamma = 1 / L
-
-    pepit_tau, theoretical_tau = wc_gd(L=L,
-                                       gamma=gamma,
-                                       n=n)
-
+    pepit_tau, theoretical_tau = wc_gd(L=L, gamma=1 / L, n=5, verbose=True)
