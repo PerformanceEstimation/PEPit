65da838b72a9bb75b75f2c217609feb4941abb07
baptiste <baptiste.goujaud@gmail.com>
2021-12-21 22:37:52 +0100

    Fixed point docstring

--- PEPit/primitive_steps/fixed_point.py
+++ PEPit/primitive_steps/fixed_point.py
@@ -4,19 +4,24 @@ from PEPit.expression import Expression
 
 def fixed_point(A):
     """
-    This routine allows to find a fixed point of an operator A, that is x suh that:
-    Ax = x.
+    This routine allows to find a fixed point of the operator :math:`A`, that is :math:`x` such that :math:`Ax = x`.
 
     Args:
-        A (Function): an operator or a function
+        A (Function): an operator or a function.
 
     Returns:
-         - if A is an operator, x such that x = A x (fixed point of A)
-         - if A is a function, x such that x = A.gradient(x)
+        x (Point): a fixed point of A.
+        Ax (Point): Ax = x.
+        fx (Expression): a function value (useful only if A is a function).
 
     """
 
+    # Define a point and function value
     x = Point()
     fx = Expression()
+
+    # Add triplet to A list of points (by definition Ax = x)
     A.add_point((x, x, fx))
+
+    # Return the aforementioned triplet
     return x, x, fx
