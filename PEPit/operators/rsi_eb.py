70637f4a9c872d6e7a3b9ca5b3c05c8e8b8678a9
baptiste <baptiste.goujaud@gmail.com>
2022-06-08 01:20:01 +0200

    Fix typo

--- PEPit/operators/rsi_eb.py
+++ PEPit/operators/rsi_eb.py
@@ -44,7 +44,7 @@ class RsiEbOperator(Function):
             mu (float): The restricted secant inequality parameter.
             L (float): The upper error bound parameter.
             is_leaf (bool): True if self is defined from scratch.
-                            False is self is defined as linear combination of leaf .
+                            False if self is defined as linear combination of leaf .
             decomposition_dict (dict): decomposition of self as linear combination of leaf :class:`Function` objects.
                                        Keys are :class:`Function` objects and values are their associated coefficients.
             reuse_gradient (bool): If True, the same subgradient is returned
