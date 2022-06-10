eab83af8e076e5a7ac190451ae7427460ac4f474
baptiste <baptiste.goujaud@gmail.com>
2022-06-10 04:19:26 +0200

    very small formulation change based on Adrien suggestion

--- PEPit/functions/convex_indicator.py
+++ PEPit/functions/convex_indicator.py
@@ -9,7 +9,7 @@ class ConvexIndicatorFunction(Function):
     implementing interpolation constraints for the class of closed convex indicator functions.
 
     Attributes:
-        D (float): upper bound on the diameter of the feasible set, that can be infinite
+        D (float): upper bound on the diameter of the feasible set, possibly set to np.inf
 
     Convex indicator functions are characterized by a parameter `D`, hence can be instantiated as
 
