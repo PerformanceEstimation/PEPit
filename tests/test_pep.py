e74e9699abddf1c3da75e9a45ac3df75cf58e371
baptiste <baptiste.goujaud@gmail.com>
2022-09-16 05:32:52 +0200

    Slight cosmetic modifications of test_pep.py file

--- tests/test_pep.py
+++ tests/test_pep.py
@@ -130,7 +130,7 @@ class TestPEP(unittest.TestCase):
         # The value stored in expr must be equal to the optimal value of this SDP.
         self.assertAlmostEqual(expr.eval(), pepit_tau, delta=pepit_tau * 10 ** -3)
 
-    def test_trace_trick(self):
+    def test_dimension_reduction(self):
 
         # Compute pepit_tau very basically
         pepit_tau = self.problem.solve(verbose=0)
@@ -147,12 +147,12 @@ class TestPEP(unittest.TestCase):
         # Verify that, even with dimension reduction (using trace heuristic),
         # the solve method returns the worst-case performance, not the chosen heuristic value.
         pepit_tau2 = self.problem.solve(verbose=0, dimension_reduction_heuristic="trace")
-        self.assertAlmostEqual(pepit_tau, pepit_tau2, delta=10 ** -2)
+        self.assertAlmostEqual(pepit_tau2, pepit_tau, delta=10 ** -2)
 
         # Verify that, even with dimension reduction (using 2 steps of local regularization of the log det heuristic),
         # the solve method returns the worst-case performance, not the chosen heuristic value.
         pepit_tau3 = self.problem.solve(verbose=0, dimension_reduction_heuristic="logdet2")
-        self.assertAlmostEqual(pepit_tau, pepit_tau3, delta=10 ** -2)
+        self.assertAlmostEqual(pepit_tau3, pepit_tau, delta=10 ** -2)
 
     def tearDown(self):
 
