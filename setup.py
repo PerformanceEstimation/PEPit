3a35765fd4eca3e94c277dd5b5ac52ede8ca8396
baptiste <baptiste.goujaud@gmail.com>
2022-09-21 04:59:29 +0200

    Prepare README, quickstart and setup.py for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.1.0"
+version = "0.2.0"
 
 setuptools.setup(
     name="PEPit",
