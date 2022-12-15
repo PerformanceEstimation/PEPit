39b2995d6afd42f6be3738ba93d79f323643fbff
baptiste <baptiste.goujaud@gmail.com>
2022-12-15 18:57:42 +0100

    Fix some verbose display in docstrings

--- tests/additional_complexified_examples_tests/proximal_point.py
+++ tests/additional_complexified_examples_tests/proximal_point.py
@@ -18,10 +18,11 @@ def wc_proximal_point_complexified(gamma, n, verbose=1):
         gamma (float): the step size parameter.
         n (int): number of iterations.
         verbose (int): Level of information details to print.
-                       -1: No verbose at all.
-                       0: This example's output.
-                       1: This example's output + PEPit information.
-                       2: This example's output + PEPit information + CVXPY details.
+
+                        - -1: No verbose at all.
+                        - 0: This example's output.
+                        - 1: This example's output + PEPit information.
+                        - 2: This example's output + PEPit information + CVXPY details.
 
     Returns:
         tuple: worst_case value, theoretical value
