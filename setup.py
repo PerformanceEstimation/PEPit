badb720fb9279e7b8dac24196e9f6cd23c49d2f1
baptiste <baptiste.goujaud@gmail.com>
2024-01-05 16:24:49 +0100

    Update doc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "0.3.0"
+version = "0.3.1"
 
 setuptools.setup(
     name="PEPit",
