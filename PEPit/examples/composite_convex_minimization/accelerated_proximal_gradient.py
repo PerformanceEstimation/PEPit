5f37bfb617a536010d68bfbfcce8124ef31833b9
baptiste <baptiste.goujaud@gmail.com>
2025-08-10 00:15:28 +0200

    Small fix

--- PEPit/examples/composite_convex_minimization/accelerated_proximal_gradient.py
+++ PEPit/examples/composite_convex_minimization/accelerated_proximal_gradient.py
@@ -1,9 +1,10 @@
+from math import sqrt
+
 from PEPit import PEP
 from PEPit.functions import SmoothStronglyConvexFunction
 from PEPit.functions import ConvexFunction
 from PEPit.primitive_steps import proximal_step
 
-import numpy as np
 
 def wc_accelerated_proximal_gradient(mu, L, n, wrapper="cvxpy", solver=None, verbose=1):
     """
@@ -27,19 +28,19 @@ def wc_accelerated_proximal_gradient(mu, L, n, wrapper="cvxpy", solver=None, ver
     :math:`\\tau(n, L, \\mu)` is computed as the worst-case value of
     :math:`F(x_n) - F(x_\\star)` when :math:`\\|x_0 - x_\\star\\|^2 \\leqslant 1`.
 
-    **Algorithm**: Initialize :math:`\\lambda_1=1`, :math:`y_1=x_0`. One iteration of FISTA is described by
+    **Algorithm**: Initialize :math:`\\lambda_0=1`, :math:`y_1=x_0`. One iteration of FISTA is described by
 
     .. math::
 
         \\begin{eqnarray}
-            \\text{Set: }\\lambda_{t+1} & = & \\frac{1}{2} \\left(1 + \\sqrt{4\\lambda_t^2 + 1}\\right)\\\\
-            x_{t} & = & \\arg\\min_x \\left\\{h(x)+\\frac{L}{2}\|x-\\left(y_{t} - \\frac{1}{L} \\nabla f(y_t)\\right)\\|^2 \\right\\}\\\\
-            y_{t+1} & = & x_{t} + \\frac{\\lambda_t-1}{\\lambda_{t+1}} (x_t-x_{t-1}).
+            \\text{Set: }\\lambda_{t+1} & = & \\frac{1 + \\sqrt{4\\lambda_t^2 + 1}}{2}\\\\
+            x_t & = & \\arg\\min_x \\left\\{h(x)+\\frac{L}{2}\|x-\\left(y_t - \\frac{1}{L} \\nabla f(y_t)\\right)\\|^2 \\right\\}\\\\
+            y_{t+1} & = & x_t + \\frac{\\lambda_t-1}{\\lambda_{t+1}} (x_t-x_{t-1}).
         \\end{eqnarray}
 
     **Theoretical guarantee**: The following worst-case guarantee can be found in e.g., [1, Theorem 4.4]:
 
-    .. math:: f(x_n)-f_\\star \\leqslant \\frac{L\\|x_0-x_\\star\\|^2}{\\lambda_n^2}.
+    .. math:: f(x_n)-f_\\star \\leqslant \\frac{L}{2}\\frac{\\|x_0-x_\\star\\|^2}{\\lambda_n^2}.
 
     **References**:
     
@@ -73,26 +74,26 @@ def wc_accelerated_proximal_gradient(mu, L, n, wrapper="cvxpy", solver=None, ver
         (PEPit) Setting up the problem: Adding initial conditions and general constraints ...
         (PEPit) Setting up the problem: initial conditions and general constraints (1 constraint(s) added)
         (PEPit) Setting up the problem: interpolation conditions for 2 function(s)
-        			Function 1 : Adding 30 scalar constraint(s) ...
-        			Function 1 : 30 scalar constraint(s) added
-        			Function 2 : Adding 20 scalar constraint(s) ...
-        			Function 2 : 20 scalar constraint(s) added
+                    Function 1 : Adding 30 scalar constraint(s) ...
+                    Function 1 : 30 scalar constraint(s) added
+                    Function 2 : Adding 20 scalar constraint(s) ...
+                    Function 2 : 20 scalar constraint(s) added
         (PEPit) Setting up the problem: additional constraints for 0 function(s)
         (PEPit) Compiling SDP
         (PEPit) Calling SDP solver
-        (PEPit) Solver status: optimal (wrapper:cvxpy, solver: MOSEK); optimal value: 0.05167329605152958
+        (PEPit) Solver status: optimal (wrapper:cvxpy, solver: MOSEK); optimal value: 0.045363656016817494
         (PEPit) Primal feasibility check:
-        		The solver found a Gram matrix that is positive semi-definite up to an error of 6.64684463996332e-09
-        		All the primal scalar constraints are verified up to an error of 1.6451693951591295e-08
+                The solver found a Gram matrix that is positive semi-definite up to an error of 7.3555132913319e-09
+                All the primal scalar constraints are verified up to an error of 1.7867120057774022e-08
         (PEPit) Dual feasibility check:
-        		The solver found a residual matrix that is positive semi-definite
-        		All the dual scalar values associated with inequality constraints are nonnegative
-        (PEPit) The worst-case guarantee proof is perfectly reconstituted up to an error of 8.587603813802402e-08
-        (PEPit) Final upper bound (dual): 0.051673302055698395 and lower bound (primal example): 0.05167329605152958 
-        (PEPit) Duality gap: absolute: 6.004168814910393e-09 and relative: 1.1619480996379491e-07
+                The solver found a residual matrix that is positive semi-definite
+                All the dual scalar values associated with inequality constraints are nonnegative up to an error of 5.255603842175434e-10
+        (PEPit) The worst-case guarantee proof is perfectly reconstituted up to an error of 9.450420430196857e-08
+        (PEPit) Final upper bound (dual): 0.04536366234093697 and lower bound (primal example): 0.045363656016817494
+        (PEPit) Duality gap: absolute: 6.32411947809608e-09 and relative: 1.394093870157106e-07
         *** Example file: worst-case performance of the Accelerated Proximal Gradient Method in function values***
-        	PEPit guarantee:	 f(x_n)-f_* <= 0.0516733 ||x0 - xs||^2
-        	Theoretical guarantee:	 f(x_n)-f_* <= 0.0661257 ||x0 - xs||^2
+            PEPit guarantee:	 f(x_n)-f_* <= 0.0453637 ||x0 - xs||^2
+            Theoretical guarantee:	 f(x_n)-f_* <= 0.0460565 ||x0 - xs||^2
     
     """
 
@@ -117,15 +118,15 @@ def wc_accelerated_proximal_gradient(mu, L, n, wrapper="cvxpy", solver=None, ver
     # Compute n steps of the accelerated proximal gradient method starting from x0    	
     x_new = x0
     y = x0
-    lam = 1
+    lam = (1 + sqrt(5)) / 2
     for i in range(n):
         lam_old = lam
-        lam = (1 + np.sqrt(4 * lam_old ** 2 + 1)) / 2
+        lam = (1 + sqrt(4 * lam_old ** 2 + 1)) / 2
         x_old = x_new
         x_new, _, hx_new = proximal_step(y - 1 / L * f.gradient(y), h, 1 / L)
-        y = x_new + (lam_old - 1) / lam * (x_new - x_old) 
+        y = x_new + (lam_old - 1) / lam * (x_new - x_old)
 
-    # Set the performance metric to the function value accuracy
+        # Set the performance metric to the function value accuracy
     problem.set_performance_metric((f(x_new) + hx_new) - Fs)
 
     # Solve the PEP
@@ -133,8 +134,8 @@ def wc_accelerated_proximal_gradient(mu, L, n, wrapper="cvxpy", solver=None, ver
     pepit_tau = problem.solve(wrapper=wrapper, solver=solver, verbose=pepit_verbose)
 
     # Theoretical guarantee (for comparison)
-    theoretical_tau = L / 2 / lam_old**2
-    
+    theoretical_tau = L / (2 * lam_old ** 2)
+
     if mu != 0:
         print('Warning: momentum is tuned for non-strongly convex functions.')
 
